48a61745d4c6a656171bab9daf42a7e8af82e73f
ColtenMikulastik <mikulastikc@gmail.com>
2023-07-26 12:05:19 -0500

    working making a scalable solution to trying every password procedurally

---
+++ test.py
@@ -0,0 +1,16 @@
+
+
+import math
+
+pass_len = 4
+
+alpha_list = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+test_list = [len(alpha_list) - 1] * pass_len
+
+for delta in range(0, mathsqrt(
+for letter in range(0, pass_len):
+        
+
+
+
